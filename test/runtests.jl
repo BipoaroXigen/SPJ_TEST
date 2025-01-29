61f5ef0b246ce80bc465526777e418690447e7f9
Jonas Kriz <krizjona@fel.cvut.cz>
2025-01-29 20:32:12 +0100

    kdf

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,8 @@ using Test
 @testset "EO.jl" begin
     # Write your tests here.
     @test 1 == 1
+
+    # create chromosomes
+    @test isa(EO.BinaryChromosome, EO.get_binary_chromosome(ones(10)))
+
 end
