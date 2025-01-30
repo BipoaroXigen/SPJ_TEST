ea8e3d6d60d84fa19d0145cb642577e760b9ad92
Jonas Kriz <krizjona@fel.cvut.cz>
2025-01-30 15:38:12 +0100

    kpl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,6 @@ using Test
 
 f = x->0
 
-#= objective_function  = f_onemax
-initialization      = enclose_noargs(binary_initialization, 2, 100, objective_function)
-selection           = enclose_arguments(EO.s_tournament, 100, 3)
-crossover           = enclose_arguments(EO.cr_parent_sum, pop_size)
-mutation            = enclose_arguments(gaussian_preturbation!, 0.25)
-replacement         = EO.enclose_replacement(EO.r_keep_best_n_stoch, pop_size, constraints, 0.4)
-termination         = enclose_argument(iteration_termination, pop_size*100)
-
-solution = solvink_hart(objective_function, initialization, selection, crossover, mutation, replacement, termination; constraints=constraints) =#
-
 #@testset "EO.jl" begin
 @testset "basics" begin
     # Write your tests here.
@@ -28,6 +18,21 @@ solution = solvink_hart(objective_function, initialization, selection, crossover
     # population initialization
     @test EO.binary_initialization(2, 100, f).size == 100
     @test EO.interval_real_initialization(2, 100, f, 99, 100).size == 100
+end
 
+dims = 10
+pop_size = 100
+objective_function  = f_onemax
+initialization      = enclose_noargs(binary_initialization, 10, pop_size, objective_function)
+selection           = enclose_arguments(EO.s_tournament, pop_size, 3)
+crossover           = enclose_arguments(EO.cr_single_point, pop_size)
+mutation            = enclose_arguments(basic_preturbation!, 0.25)
+replacement         = EO.enclose_replacement(EO.r_keep_best_n_stoch, pop_size, constraints, 0.4)
+termination         = enclose_argument(iteration_termination, pop_size*100)
+solution = solvink_hart(objective_function, initialization, selection, crossover, mutation, replacement, termination; constraints=constraints)
+
+@testset "LS" begin
+    
+    @test solution.top_value < dims
 
 end
