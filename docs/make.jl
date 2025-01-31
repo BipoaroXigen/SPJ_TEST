94e1a7292d90e77d2018800d262820fc111ee773
Jonas Kriz <krizjona@fel.cvut.cz>
2025-01-31 02:21:21 +0100

    kpl

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ makedocs(
 )
 
 deploydocs(
-    #repo = "https://github.com/BipoaroXigen/SPJ_TEST",  # Replace with your repo URL
-    #branch = "gh-pages",  # This is the branch where the documentation will be hosted
-    target = "build"      # The build directory where documentation is generated
+    repo = "https://github.com/BipoaroXigen/SPJ_TEST",  # Replace with your repo URL
+    branch = "gh-pages",  # This is the branch where the documentation will be hosted
+    #target = "build"      # The build directory where documentation is generated
 )
